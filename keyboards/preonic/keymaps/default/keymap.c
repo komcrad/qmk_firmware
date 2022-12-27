b0ff9e0cfd15251054cc48af8a5b84a173fcfc55
komcrad <konradt95@gmail.com>
2022-12-27 14:08:48 -0600

    Toggle vim instead of momentary on

--- keyboards/preonic/keymaps/default/keymap.c
+++ keyboards/preonic/keymaps/default/keymap.c
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,   KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_DEL,
-  KC_ESC,   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_ENT,
-  KC_LCTL,  KC_LALT, KC_LGUI, MO(_VIM), LOWER,   KC_SPC,  KC_SPC,  RAISE,   MO(_VIM), KC_DOWN, KC_UP,   CW_TOGG
+  KC_GRV,   KC_1,     KC_2,    KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_DEL,
+  KC_ESC,   KC_A,     KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_ENT,
+  CW_TOGG,  KC_LCTL,  KC_LALT, KC_LGUI,  LOWER,   KC_SPC,  KC_SPC,  RAISE,   TO(_VIM), KC_DOWN, KC_UP,   CW_TOGG
 ),
 
 /* Colemak
@@ -167,11 +167,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_VIM] = LAYOUT_preonic_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, NXTWORD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
-  _______, _______,  KC_DEL, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______,
+  _______, _______, NXTWORD, _______, _______, _______, _______, _______, TO(_QWERTY), _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,       KC_UP, KC_RGHT, _______, _______,
+  _______, _______,  KC_DEL, _______, _______, _______, _______, _______,     _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______
 )
 
 };
