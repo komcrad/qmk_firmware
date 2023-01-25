108a23c4f82550508f2391eabd40a9b62a0dccaa
komcrad <konradt95@gmail.com>
2023-01-24 22:33:57 -0600

    Turn off lighting. Fix ctrl key

--- keyboards/planck/keymaps/vim/keymap.c
+++ keyboards/planck/keymaps/vim/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    CW_TOGG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LALT, KC_SPC, RAISE, TO(_VIM), KC_DOWN, KC_RCTL, CW_TOGG
+    CW_TOGG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LCTL, KC_SPC, RAISE, TO(_VIM), KC_DOWN, KC_RCTL, CW_TOGG
 ),
 
 /* Colemak
